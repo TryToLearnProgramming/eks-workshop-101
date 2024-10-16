6273e1e5f33d22f589f4a83a5c5ffb3e03345f25
sami <samivivake@gmail.com>
2024-10-16 16:08:34 +0530

    update-version: eks module and eks-blueprints-addons module; move: default addons from eks_addons to cluster_addons

--- eks-terraform/eks-addons.tf
+++ eks-terraform/eks-addons.tf
@@ -7,6 +7,12 @@ module "eks_blueprints_addons" {
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
 
+  eks_addons = {
+    aws-ebs-csi-driver = {
+      most_recent              = true
+      service_account_role_arn = module.ebs_csi_driver_irsa.iam_role_arn
+    }
+  }
   # eks_addons = {
   #   aws-ebs-csi-driver = {
   #     most_recent = true
@@ -34,4 +40,22 @@ module "eks_blueprints_addons" {
   tags = {
     Environment = "dev"
   }
+}
+
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.20"
+
+  role_name_prefix = "${local.name}-ebs-csi-driver-"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  tags = local.tags
 }
